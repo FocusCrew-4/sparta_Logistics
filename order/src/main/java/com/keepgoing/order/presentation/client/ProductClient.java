5a1138850d8cc71641d147c27f5faf9af566efa4
yoo20370 <yoo20370@gmail.com>
2025-11-10 12:57:09 +0900

    fix(Order) : 상품, 허브 서비스와 통신할 때 주고 받을 데이터 수정 및 그에 따른 OrderProcessor 수정

--- order/src/main/java/com/keepgoing/order/presentation/client/ProductClient.java
+++ order/src/main/java/com/keepgoing/order/presentation/client/ProductClient.java
@@ -1,6 +1,7 @@
 package com.keepgoing.order.presentation.client;
 
 import com.keepgoing.order.presentation.dto.response.ProductInfo;
+import com.keepgoing.order.presentation.dto.response.ProductSearchResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "product-service")
 public interface ProductClient {
 
-    @GetMapping("/v1/products/{id}")
-    ProductInfo getProduct(@PathVariable(name = "id") String id);
+    @GetMapping("/v1/internal/products/{id}")
+    ProductSearchResponse getProduct(@PathVariable(name = "id") String id);
 }
