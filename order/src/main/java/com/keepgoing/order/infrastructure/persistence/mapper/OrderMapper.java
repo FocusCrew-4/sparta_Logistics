2a9bc4c4505e33a748771a8246c77848635ee2c3
yoo20370 <yoo20370@gmail.com>
2025-11-16 01:40:27 +0900

    feat : 도메인 엔티티와 영속성 엔티티를 위한 Mapper 클래스 추가

---
+++ order/src/main/java/com/keepgoing/order/infrastructure/persistence/mapper/OrderMapper.java
@@ -0,0 +1,49 @@
+package com.keepgoing.order.infrastructure.persistence.mapper;
+
+import com.keepgoing.order.domain.model.Order;
+import com.keepgoing.order.infrastructure.persistence.order.entity.OrderEntity;
+
+public final class OrderMapper {
+
+    private OrderMapper() {
+
+    }
+
+    public static OrderEntity toEntityForCreate(Order order) {
+        return OrderEntity.create(
+            order.getMember().memberId(),
+            order.getSupplier().vendorId(),
+            order.getReceiver().vendorId(),
+            order.getProduct().productId(),
+            order.getDelivery().deliveryDueAt(),
+            order.getDelivery().deliveryRequestNote(),
+            order.getQuantity(),
+            order.getTotalPrice(),
+            order.getOrderState(),
+            order.getCancelState(),
+            order.getIdempotencyKey(),
+            order.getOrderedAt()
+        );
+    }
+
+    public static Order toDomain(OrderEntity orderEntity) {
+        return Order.of(
+            orderEntity.getId(),
+            orderEntity.getMember().getMemberId(),
+            orderEntity.getSupplier().getVendorId(),
+            orderEntity.getReceiver().getVendorId(),
+            orderEntity.getProduct().getProductId(),
+            orderEntity.getHub().getHubId(),
+            orderEntity.getDelivery().getDeliveryId(),
+            orderEntity.getDelivery().getDeliveryDueAt(),
+            orderEntity.getDelivery().getDeliveryRequestNote(),
+            orderEntity.getQuantity(),
+            orderEntity.getTotalPrice(),
+            orderEntity.getOrderState(),
+            orderEntity.getCancelState(),
+            orderEntity.getIdempotencyKey(),
+            orderEntity.getOrderedAt(),
+            orderEntity.getConfirmedAt(),
+            orderEntity.getCancelledAt());
+    }
+}
